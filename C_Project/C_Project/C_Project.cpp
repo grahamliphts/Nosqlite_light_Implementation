ba0736e92c6434d0a2d5345396f982c919d3cb4c
Kevin Rey <k.rey@hotmail.fr>
2015-07-21 17:16:31 +0200

    Ajout Fonctions Cours

--- C_Project/C_Project/C_Project.cpp
+++ C_Project/C_Project/C_Project.cpp
@@ -8,11 +8,30 @@
 #include "input.h"
 
 
-int main(void){
+int main(int argc, char** argv)
+{
+	if (argc < 3)
+	{
+		printf("Pas assez de parametres");
+		return 1;
+	}
+
 	char* line;
-	do{
-	line = getline();
-	printf("%s\n", line);
-	} while (strcmp(line, "exit\n") != 0);
+
+	do
+	{
+		line = getline();
+		printf("%s\n", line);
+
+		if (line[0] == '{')
+		{
+			while (line[0] != '}' || strcmp(line, "exit\n") != 0)
+			{
+
+			}
+		}
+	} 
+	while (strcmp(line, "exit\n") != 0);
+	
 	return 0;
 }
--- C_Project/C_Project/input.cpp
+++ C_Project/C_Project/input.cpp
@@ -3,24 +3,28 @@
 #include <stdio.h>
 #include "input.h"
 
-char * getline(void) {
-	char * line = (char*)malloc(100), *linep = line;
+char* getline(void) 
+{
+	char* line = (char*)malloc(100), *linep = line;
 	size_t lenmax = 100, len = lenmax;
 	int c;
 
 	if (line == NULL)
 		return NULL;
 
-	for (;;) {
+	for (;;) 
+	{
 		c = fgetc(stdin);
 		if (c == EOF)
 			break;
 
-		if (--len == 0) {
+		if (--len == 0) 
+		{
 			len = lenmax;
 			char * linen = (char*)realloc(linep, lenmax *= 2);
 
-			if (linen == NULL) {
+			if (linen == NULL) 
+			{
 				free(linep);
 				return NULL;
 			}
